eb09a9ea03a2d5b2a98c328610cd269ee1e5d661
Jopar <118544939+xJopar@users.noreply.github.com>
2022-11-20 16:47:28 -0300

    fix bugs on modelo.py

--- modelo.py
+++ modelo.py
@@ -23,10 +23,13 @@ class Jugador(Persona):
                 victoria= self.pokemon.atacar(poke_rival)
                 if victoria:
                     print("{} gano la batalla.".format(self))
+                    poke_rival.vida=poke_rival.vida_max
+                    print("{} va a llevar a su pokemon a curar.".format(randomEnemigo.nombre))
                     break
                 derrota = poke_rival.atacar(self.pokemon)
                 if derrota:
-                    print("{} gano la batalla, tu perdiste".format(randomEnemigo.nombre))
+                    print("{} gano la batalla, tu perdiste. Te vas triste a curar a tu pokemon".format(randomEnemigo.nombre))
+                    self.curar_pokemon()
                     break            
     def curar_pokemon(self):
         print("Curando a tu {} al 100%".format(self.pokemon.nickname))
@@ -39,18 +42,15 @@ class Pokemon:
     def __init__(self,especie,nickname=None):
         #Si el nickname retorna falso poner como nickname la especie
         self.especie=especie
-        if nickname:
-            self.nickname= nickname
-        else:
-            self.nickname= especie
 
     def atacar(self,pokerival):
-        if   self.especie == pokerival.debilidad:
-            pokerival.vida -= int(self.danho * 1.3)
-            print("{} hizo {} de daño a {}, es un ataque efectivo.\n".format(self.nickname, int(self.danho * 1.3) ,pokerival.especie))
+        if   self.tipo == pokerival.debilidad:
+            pokerival.vida -= int(self.danho + 1)
+            print("{} hizo {} de daño a {}, es un ataque efectivo. \n".format(self.nickname, int(self.danho + 1) ,str(pokerival.especie)))
         else:
             pokerival.vida -= int(self.danho)
-            print("{} hizo {} de daño a {}, es un ataque normal.\n".format(self.nickname, int(self.danho) ,pokerival.especie))
+            print("{} hizo {} de daño a {}, es un ataque normal.\n".format(self.nickname, int(self.danho) ,str(pokerival.especie)))
+
         if pokerival.vida <=0:
             print("{} fue derrotado".format(pokerival.especie))
             return True
@@ -58,19 +58,27 @@ class Pokemon:
             return False  
 
 class Tipo_Fuego(Pokemon):
-    def __init__(self, especie,nickname=None):  
+    def __init__(self, especie,nickname=None):
+        if nickname:
+            self.nickname= nickname
+        else:
+            self.nickname= especie         
         super().__init__(especie,nickname=None)      
-    tipo="fuego"
+    tipo= "fuego"
     danho= random.randint(5,8)
     vida=random.randint(5,10)
-    debilidad="agua"
+    debilidad= "agua"
     vida_max= vida    
     def atacar(self, pokemon):
         print("{} Usó aliento de fuego en {}!".format(self.especie, pokemon.especie))
         return super().atacar(pokemon)
 
 class Tipo_Agua(Pokemon):
-    def __init__(self, especie,nickname=None):  
+    def __init__(self, especie,nickname=None): 
+        if nickname:
+            self.nickname= nickname
+        else:
+            self.nickname= especie   
         super().__init__(especie,nickname=None)  
     tipo="agua"
     danho= random.randint(2,5)
@@ -83,6 +91,10 @@ class Tipo_Agua(Pokemon):
 
 class Tipo_Planta(Pokemon):
     def __init__(self, especie,nickname=None):  
+        if nickname:
+            self.nickname= nickname
+        else:
+            self.nickname= especie 
         super().__init__(especie,nickname=None)      
     tipo="planta"
     danho= random.randint(3,6)
